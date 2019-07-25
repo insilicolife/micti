cec35b67230b4a92f1b3292546011f1ad605c16c
insilicolife <babyawet@gmail.com>
2019-07-25 12:54:18 +0000

    MICTI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MICTI",
-    version="0.1.3",
+    version="0.1.4",
     author="Nigatu Ayele",
     author_email="naodm2006awet4@gmail.com",
     description="Feature extraction approach in single-cell gene expression profiling for cell-type marker identification.",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/insilicolife/micti",
     packages=setuptools.find_packages(), 
-    install_requires=['numpy', 'pandas','scipy','scikit-learn', 'matplotlib','mpl_toolkits','pylab','gensim', 'gprofiler', 'seaborn','pyensembl', 'bs4','requests'],
+    install_requires=['numpy', 'pandas','scipy','scikit-learn', 'matplotlib','gensim', 'gprofiler', 'seaborn','pyensembl', 'bs4','requests'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
